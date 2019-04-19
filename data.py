c3b767db36f747397bd963ddfdb32a2b80cb50be
Christos Alexiou <pcnoic@gmail.com>
2019-04-19 13:40:21 +0300

    updated scripts to py3
    now using PyMySQL instead of sqlconnector

--- data.py
+++ data.py
@@ -1,42 +1,49 @@
-#!C:/Python27
+#!/usr/bin/python3
 
-import copy
-import mysql.connector
-import cgi
-import cgitb
+import copy 
+import PyMySQL 
+import cgi 
+import cgitb 
 import os
 import sys
 import sleep
 
-
-if __name__ == '__main__':
-    print "Content-type:text/html\n\n"
-    print "Starting connection to the database" 
-    sleep.time() 
+if __name__='__name__':
+  print('Content-type:text/html\n\n') 
+  print('Starting connection to the database') 
+  
+  sleep.time(3) 
+  
+  #getting database credentials 
+  usr = input('Please, enter DB username: ')
+  dbname = input('Enter, DB name: ')
+  dbpass = input('Enter, DB password: ') 
+  dbsrv = input('Enter, database address: ') 
+  
+  
+  
+  try:
+    con = PyMySQL.connect(dbsrv, usr, dbpass, dbname) 
+    cursor = con.cursor() #initiating the cursor 
+    idq = 4 
+    name = "test" #enter your name 
+    sqlQ1 = "INSERT INTO data(id, name) VALUES ('%d','%s')" %(idq, name)
+   except: 
+    print('cannot connect to sql, exiting...')
+    sleep.time(5)
+    exit()
     
-    try:
-		conn = mysql.connector.connect(user='root',password='',host='localhost',database='abcd')
-		cursor = conn.cursor()
-		idq=4
-		name="christos"
-		sql1 = "INSERT INTO data(id,name) VALUES ('%d','%s')" %(idq,name)
-		print sql1
-	except:
-		# error message in case connection to sql is not available 
-		print "cannot connect to sql. exiting..." 
-		sleep.time(2) 
-		exit() 
-		
-    try:
-       # Execute the SQL command
-       cursor.execute(sql1)
-       # Commit your changes in the database
-       conn.commit()
-       print "added"
-    except:
-       # Rollback in case there is any error
-       conn.rollback()
-
-
-    conn.close()
-
+    
+   #trying to execute SQL script 
+  try:
+    cursor.execute(sqlQ1) #execution 
+    con.commit() #commiting changes 
+    print('ADDED') 
+   except: 
+    con.rollback() #revert changes in case of failure 
+   
+  
+  con.close() 
+  
+  
+    
--- getlist.py
+++ getlist.py
@@ -1,37 +1,41 @@
-#!C:/Python27/python
+#!/usr/bin/python3 
 
-
-import cgi
-import cgitb
+import cgi 
+import cgitb 
 import os
-import mysql.connector
-import datetime
+import PyMySQL 
+import datetime 
 import time
 import json
-import sys
+import sys 
+
+usr = input('Please, enter DB username: ')
+dbname = input('Enter, DB name: ')
+dbpass = input('Enter, DB password: ') 
+dbsrv = input('Enter, database address: ') 
 
 
-
-
-conn = mysql.connector.connect(user='root',password='',host='localhost',database='abcd')
-
-cursor = conn.cursor()
-sql="select * from data order by name asc"
-cursor.execute(sql)
-results = cursor.fetchall()
-list1=[]
-for row in results:
-         name = row[1]
-         print name
-         
-
-conn.commit()
-conn.close()
-
-
-
-
-
-
-
-
+try:
+  con = PyMySQL.connect(dbsrv, dbname, dbpass, dbname) #attempoting connection to SQL 
+  cursor = con.cursor() 
+  
+  query = "SELECT * FROM DATA ORDER BY NAME ASC" 
+  
+  cursor.execute(query) 
+  results = cursor.fetchall() 
+  
+  listUNO = []
+  
+  for row in results:
+    name = row[1] 
+    print name 
+    
+   
+  con.commit() #commiting to database 
+  con.close() #exiting 
+  
+  
+  
+  
+  
+  
